5ffcfe6db937f6f1005b7fd7e862e0d158aadd14
Sujal Shakya <sujalshakya387@gmail.com>
2024-12-19 09:36:12 +0545

    feat: check if testmode or not while generating pidx

--- lib/khalti/helper.dart
+++ lib/khalti/helper.dart
@@ -1,40 +1,34 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:payment_gateway_package/khalti/khalti_pidx_request.dart';
+import 'package:payment_gateway_package/khalti/khalti_pidx_response.dart';
 
-Future<String?> generatePidx() async {
-  const String khaltiApiUrl = 'https://a.khalti.com/api/v2/epayment/initiate/';
-
-  final Map<String, dynamic> requestPayload = {
-    "return_url": "http://google.com/",
-    "website_url": "http://google.com/",
-    "amount": "1000",
-    "purchase_order_id": "Order01",
-    "purchase_order_name": "Test",
-    "customer_info": {
-      "name": "Test",
-      "email": "test@khalti.com",
-      "phone": "9800000001"
-    }
-  };
+Future<KhaltiPidxResponse?> generatePidx(
+    KhaltiPidxRequest request, bool testMode) async {
+  String khaltiApiUrl = testMode
+      ? 'https://a.khalti.com/api/v2/epayment/initiate/'
+      : 'https://khalti.com/api/v2/epayment/initiate/';
 
   try {
     final response = await http.post(
       Uri.parse(khaltiApiUrl),
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Key 6465d7e60f3549ad93a49e61949fd94a"
+        "Authorization": "Key 6465d7e60f3549ad93a49e61949fd94a",
       },
-      body: jsonEncode(requestPayload),
+      body: jsonEncode(request.toJson()),
     );
+
     if (response.statusCode == 200) {
-      final responseBody = jsonDecode(response.body);
-      debugPrint(responseBody['pidx']);
-      return responseBody['pidx']; // Return the generated pidx
+      final Map<String, dynamic> responseBody = jsonDecode(response.body);
+      return KhaltiPidxResponse.fromJson(responseBody);
     } else {
+      debugPrint("Error: ${response.statusCode} - ${response.body}");
       return null;
     }
   } catch (e) {
+    debugPrint("Exception: $e");
     return null;
   }
 }
